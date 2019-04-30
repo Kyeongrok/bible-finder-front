66e81dd78576efec37f8edfe99be33735e718380
mattheu <mattheu@plutusds.com>
2019-04-30 18:04:07 +0900

    to 45

--- src/component/SeparateSit.js
+++ src/component/SeparateSit.js
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
+import seats from './Seats';
 
 const Ttable=seats=> {
-  const teamMap = {1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:"", 9:"", 10:""}
+  const teamMap = {1:"경록", 2:"진주", 3:"윤준", 4:"창환", 5:"조총", 6:"다은", 7:"송", 8:"상수", 9:"예나", 10:"은진"}
   console.log(seats.seats);
   return(
-    <table border="1px">
-      <tbody>
-      <tr>
-        <td></td>
-        <td>{seats.seats.week}</td>
-        <td>주차</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td>{seats.seats.seats[0]}</td>
-        <td>{seats.seats.seats[3]}</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td>{seats.seats.seats[1]}</td>
-        <td>{seats.seats.seats[4]}</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td>{seats.seats.seats[2]}</td>
-        <td>{seats.seats.seats[5]}</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>{seats.seats.seats[6]}</td>
-        <td>{seats.seats.seats[7]}</td>
-        <td>{seats.seats.seats[8]}</td>
-        <td>{seats.seats.seats[9]}</td>
-      </tr>
-      </tbody>
-
-
-    </table>
+    <div>
+      <br/>
+      <table border="1px">
+        <tbody>
+        <tr>
+          <td></td>
+          <td>{seats.seats.week}</td>
+          <td>주차</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>{teamMap[seats.seats.seats[0]]}</td>
+          <td>{teamMap[seats.seats.seats[3]]}</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>{teamMap[seats.seats.seats[1]]}</td>
+          <td>{teamMap[seats.seats.seats[4]]}</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>{teamMap[seats.seats.seats[2]]}</td>
+          <td>{teamMap[seats.seats.seats[5]]}</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>{teamMap[seats.seats.seats[6]]}</td>
+          <td>{teamMap[seats.seats.seats[7]]}</td>
+          <td>{teamMap[seats.seats.seats[8]]}</td>
+          <td>{teamMap[seats.seats.seats[9]]}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
@@ -48,20 +50,11 @@ class SeparateSit extends Component{
     super(props);
   }
 
-  getSeats() {
-    const seats = {"items":[
-        {"week":9, "seats":[1, 2, 6, 10, 8, 3, 4, 5, 7, 9]},
-      ]}
-    return seats;
-  }
-
   render(){
-    const seats = this.getSeats();
-
     return (
       <div>
         자리배치
-        <Ttable seats={seats.items[0]}/>
+        {seats.items.map((item, key)=><Ttable seats={item}/>)}
       </div>
     );
   }
