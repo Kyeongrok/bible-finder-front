25e2f19ef883aae738862dcbe85f59463f6789ed
mattheu <mattheu@plutusds.com>
2019-05-07 16:47:48 +0900

    json integration

--- src/component/BibleFinder.js
+++ src/component/BibleFinder.js
@@ -4,16 +4,27 @@ import Amplify, { Auth } from 'aws-amplify';
 import axios from 'axios';
 
 class BibleFinder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: ""};
+  }
   componentDidMount(){
     console.log("---");
-    const url = "http://biblefinder.co.kr:5000/find-single/%EC%B0%BD1:1";
+    const url = "http://biblefinder.co.kr:5000/json/find-single/%EC%B0%BD1:1";
     axios.get(url)
-      .then(response=>console.log(response))
+      .then(response=>{
+        console.log(response);
+        this.setState({"data":response.data})
+      })
       .catch(error=>console.log(error))
   }
   render(){
+    console.log(this.state.data);
     return(
-      <div>hellooo</div>
+      <div>
+        찾을곳:<input type="text"/> ex) 딤후3:16<br/>
+        {`${this.state.data.index} ${this.state.data.text}`}
+      </div>
     )
 
   }
