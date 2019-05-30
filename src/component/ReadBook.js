326d2bed4f9c0b6cbca9d7802d1a138e83aad6b8
mattheu <mattheu@plutusds.com>
2019-05-30 17:57:01 +0900

    add search box

--- src/component/ReadBook.js
+++ src/component/ReadBook.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import {Button, Card, Container} from "react-bootstrap";
+import {Button, Card, Container, Form} from "react-bootstrap";
 import axios from 'axios';
+import {Col} from "react-bootstrap/es";
 
 class ReadBook extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      statements:[]
+      statements:[],
+      shortenedBookName:"딤후",
+      chapter:1
     };
   }
 
   componentDidMount(){
-    this.handleChangeAddr();
+    this.handleChangeAddr("딤후", 1);
   }
 
-  handleChangeAddr() {
-    const url = `https://bag6ly7sng.execute-api.ap-northeast-2.amazonaws.com/prod/v1/find/chapter?book=${"딤후"}&chapter=${1}`
+  handleChangeAddr(book, chapter) {
+    const url = `https://bag6ly7sng.execute-api.ap-northeast-2.amazonaws.com/prod/v1/find/chapter?book=${book}&chapter=${chapter}`
     axios.get(url)
       .then(res => {
         this.setState({statements:res.data});
       });
+  }
+
+  handleChangeBook(event) {
+    this.setState({shortenedBookName: event.target.value});
+  }
+
+  handleChangeChapter(event) {
+
   }
   render(){
     console.log(this.state.statements);
@@ -27,6 +38,21 @@ class ReadBook extends Component {
     return (
       <Container>
         <Card>
+          <Card.Body>
+            <Form>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>줄인책이름</Form.Label>
+                  <Form.Control type="text" placeholder="딤후" onChange={this.handleChangeBook.bind(this)} value={this.state.shortenedBookName} />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>장</Form.Label>
+                  <Form.Control type="text" placeholder="1" onChange={this.handleChangeChapter} value={this.state.chapter} />
+                </Form.Group>
+              </Form.Row>
+            </Form>
+          </Card.Body>
           <Card.Text>
             <Button onClick={()=>this.handleChangeAddr()}>button</Button>
           </Card.Text>
